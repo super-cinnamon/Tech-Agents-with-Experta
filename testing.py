432fc3636dbd4733b0da80630330612dc454e947
super-cinnamon <67106415+super-cinnamon@users.noreply.github.com>
2022-05-17 16:08:58 +0100

    testing

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 import json
 import pprint
 
+
 pp = pprint.PrettyPrinter(indent=4)
 
 file = open("test.json")
@@ -17,18 +18,24 @@ print(type(data['object']))
 
 import time
 from spade.agent import Agent
-from spade.behaviour import OneShotBehaviour
+from spade.behaviour import OneShotBehaviour, CyclicBehaviour
 from spade.message import Message
 from spade.template import Template
 
 
 class SenderAgent(Agent):
+    def setmsg(self, message):
+            self.message = message
+    
     class InformBehav(OneShotBehaviour):
+        def set_message(self, message):
+            self.message = message
+        
         async def run(self):
             print("InformBehav running")
-            msg = Message(to="firstagentS@jix.im")     # Instantiate the message
+            msg = Message(to="myagent@jix.im")     # Instantiate the message
             msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
-            msg.body = "Hello World"                    # Set the message content
+            msg.body = self.message                    # Set the message content
 
             await self.send(msg)
             print("Message sent!")
@@ -39,13 +46,14 @@ class SenderAgent(Agent):
     async def setup(self):
         print("SenderAgent started")
         b = self.InformBehav()
+        b.set_message(self.message)
         self.add_behaviour(b)
 
 class ReceiverAgent(Agent):
-    class RecvBehav(OneShotBehaviour):
+    class RecvBehav(CyclicBehaviour):
         async def run(self):
             print("RecvBehav running")
-
+            
             msg = await self.receive(timeout=10) # wait for a message for 10 seconds
             if msg:
                 print("Message received with content: {}".format(msg.body))
@@ -53,7 +61,7 @@ class ReceiverAgent(Agent):
                 print("Did not received any message after 10 seconds")
 
             # stop agent from behaviour
-            await self.agent.stop()
+            #await self.agent.stop()
 
     async def setup(self):
         print("ReceiverAgent started")
@@ -65,17 +73,26 @@ class ReceiverAgent(Agent):
 
 
 if __name__ == "__main__":
-    receiveragent = ReceiverAgent("firstagent@jix.im", "techagent")
+    #receiveragent = ReceiverAgent("firstagent@jix.im", "techagent")
+    receiveragent = ReceiverAgent("myagent@jix.im", "techagent")
     future = receiveragent.start()
     future.result() # wait for receiver agent to be prepared.
-    senderagent = SenderAgent("mainagent@jix.im", "techagent")
+    #senderagent = SenderAgent("mainagent@jix.im", "techagent")
+
+    senderagent = SenderAgent("secondagent@jix.im", "techagent")
+    senderagent2 = SenderAgent("someagent@jix.im", "techagent")
+    senderagent.setmsg("agent1")
+    senderagent2.setmsg("agent2")
     senderagent.start()
+    future.result()
+    senderagent2.start()
 
     while receiveragent.is_alive():
         try:
-            time.sleep(1)
+            time.sleep(5)
         except KeyboardInterrupt:
             senderagent.stop()
+            senderagent2.stop()
             receiveragent.stop()
             break
     print("Agents finished")
