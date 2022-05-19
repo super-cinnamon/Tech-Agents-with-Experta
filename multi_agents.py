59447a91259ae0d4c645ff0a12c558565aaba558
super-cinnamon <67106415+super-cinnamon@users.noreply.github.com>
2022-05-19 13:32:50 +0100

    worked on it some more

--- multi_agents.py
+++ multi_agents.py
@@ -77,182 +77,189 @@ engineV = myEngine()
 ############################################### G U I ###########################################################################
 
 class Ui(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(Ui, self).__init__()
-        uic.loadUi('multi_agents.ui', self)
+	def __init__(self):
+		super(Ui, self).__init__()
+		uic.loadUi('multi_agents.ui', self)
 
-        ######################################### items declaration #######################################################
-        ################################## Buttons
-        #size button
-        self.add_size =  self.findChild(QtWidgets.QPushButton, 'add_size')
-        self.add_size.clicked.connect(self.addSizeClickListener)
+		######################################### items declaration #######################################################
+		################################## Buttons
+		#size button
+		self.add_size =  self.findChild(QtWidgets.QPushButton, 'add_size')
+		self.add_size.clicked.connect(self.addSizeClickListener)
 
-        #doors button
-        self.add_doors =  self.findChild(QtWidgets.QPushButton, 'add_doors')
-        self.add_doors.clicked.connect(self.addDoorsClickListener)
+		#doors button
+		self.add_doors =  self.findChild(QtWidgets.QPushButton, 'add_doors')
+		self.add_doors.clicked.connect(self.addDoorsClickListener)
 
-        #max temp button
-        self.add_max_temp =  self.findChild(QtWidgets.QPushButton, 'add_max_temp')
-        self.add_max_temp.clicked.connect(self.addMaxTempClickListener)
+		#max temp button
+		self.add_max_temp =  self.findChild(QtWidgets.QPushButton, 'add_max_temp')
+		self.add_max_temp.clicked.connect(self.addMaxTempClickListener)
 
-        #min temp button
-        self.add_min_temp =  self.findChild(QtWidgets.QPushButton, 'add_min_temp')
-        self.add_min_temp.clicked.connect(self.addMinTempClickListener)
+		#min temp button
+		self.add_min_temp =  self.findChild(QtWidgets.QPushButton, 'add_min_temp')
+		self.add_min_temp.clicked.connect(self.addMinTempClickListener)
 
-        #min price button
-        self.add_min_price =  self.findChild(QtWidgets.QPushButton, 'add_min_price')
-        self.add_min_price.clicked.connect(self.addMinPriceClickListener)
+		#min price button
+		self.add_min_price =  self.findChild(QtWidgets.QPushButton, 'add_min_price')
+		self.add_min_price.clicked.connect(self.addMinPriceClickListener)
 
-        #max price button
-        self.add_max_price =  self.findChild(QtWidgets.QPushButton, 'add_max_price')
-        self.add_max_price.clicked.connect(self.addMaxPriceClickListener)
+		#max price button
+		self.add_max_price =  self.findChild(QtWidgets.QPushButton, 'add_max_price')
+		self.add_max_price.clicked.connect(self.addMaxPriceClickListener)
 
-        #features button
-        self.add_feature =  self.findChild(QtWidgets.QPushButton, 'add_feature')
-        self.add_feature.clicked.connect(self.addFeatureClickListener)
+		#features button
+		self.add_feature =  self.findChild(QtWidgets.QPushButton, 'add_feature')
+		self.add_feature.clicked.connect(self.addFeatureClickListener)
 
-        #accessories button
-        self.add_accessory =  self.findChild(QtWidgets.QPushButton, 'add_accessory')
-        self.add_accessory.clicked.connect(self.addAccessoryClickListener)
+		#accessories button
+		self.add_accessory =  self.findChild(QtWidgets.QPushButton, 'add_accessory')
+		self.add_accessory.clicked.connect(self.addAccessoryClickListener)
 
-        #spinners declaration
-        self.max_temp_spinner =  self.findChild(QtWidgets.QDoubleSpinBox, 'max_temp_spinner')
-        self.min_temp_spinner =  self.findChild(QtWidgets.QDoubleSpinBox, 'min_temp_spinner')
-        self.spinner_door =  self.findChild(QtWidgets.QSpinBox, 'spinner_door')
+		#spinners declaration
+		self.max_temp_spinner =  self.findChild(QtWidgets.QDoubleSpinBox, 'max_temp_spinner')
+		self.min_temp_spinner =  self.findChild(QtWidgets.QDoubleSpinBox, 'min_temp_spinner')
+		self.spinner_door =  self.findChild(QtWidgets.QSpinBox, 'spinner_door')
 
 
-        #combo boxes declatation
-        self.combo_features =  self.findChild(QtWidgets.QComboBox, 'combo_features')
-        self.combo_size =  self.findChild(QtWidgets.QComboBox, 'combo_size')
-        self.combo_accessories =  self.findChild(QtWidgets.QComboBox, 'combo_accessories')
+		#combo boxes declatation
+		self.combo_features =  self.findChild(QtWidgets.QComboBox, 'combo_features')
+		self.combo_size =  self.findChild(QtWidgets.QComboBox, 'combo_size')
+		self.combo_accessories =  self.findChild(QtWidgets.QComboBox, 'combo_accessories')
 
-        #list view declaration with its model
-        self.reco_results = self.findChild(QtWidgets.QListView, 'reco_results')
-        self.reco_results_model = QtGui.QStandardItemModel()
-        self.reco_results.setModel(self.reco_results_model)
+		#list view declaration with its model
+		self.reco_results = self.findChild(QtWidgets.QListView, 'reco_results')
+		self.reco_results_model = QtGui.QStandardItemModel()
+		self.reco_results.setModel(self.reco_results_model)
 
-        #list view declaration with its model
-        self.facts_list = self.findChild(QtWidgets.QListView, 'facts_list')
-        self.facts_list.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
-        self.facts_list_model = QtGui.QStandardItemModel()
-        self.facts_list.setModel(self.facts_list_model)
+		#list view declaration with its model
+		self.facts_list = self.findChild(QtWidgets.QListView, 'facts_list')
+		self.facts_list.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
+		self.facts_list_model = QtGui.QStandardItemModel()
+		self.facts_list.setModel(self.facts_list_model)
 
-        ############# Tables
-        #recommendation table
-        self.shop_results = self.findChild(QtWidgets.QTableView, 'shop_results')
-        self.shop_results_model = QtGui.QStandardItemModel()
-        self.shop_results_model.setHorizontalHeaderLabels(['Name', 'price'])
-        self.shop_results.setModel(self.shop_results_model)
+		############# Tables
+		#recommendation table
+		self.shop_results = self.findChild(QtWidgets.QTableView, 'shop_results')
+		self.shop_results_model = QtGui.QStandardItemModel()
+		self.shop_results_model.setHorizontalHeaderLabels(['Name', 'price'])
+		self.shop_results.setModel(self.shop_results_model)
 
-        #cart table
-        self.cart = self.findChild(QtWidgets.QTableView, 'cart')
+		#cart table
+		self.cart = self.findChild(QtWidgets.QTableView, 'cart')
 
-        #sliders and their labels
-        #min price
-        self.min_price = self.findChild(QtWidgets.QSlider, 'min_price')
-        self.min_price_label = self.findChild(QtWidgets.QLabel, 'min_price_label')
-        self.min_price.setRange(0,200000)
-        self.min_price.valueChanged.connect(self.updateMinLabel)
+		#sliders and their labels
+		#min price
+		self.min_price = self.findChild(QtWidgets.QSlider, 'min_price')
+		self.min_price_label = self.findChild(QtWidgets.QLabel, 'min_price_label')
+		self.min_price.setRange(0,200000)
+		self.min_price.valueChanged.connect(self.updateMinLabel)
 
-        #max price
-        self.max_price = self.findChild(QtWidgets.QSlider, 'max_price')
-        self.max_price_label = self.findChild(QtWidgets.QLabel, 'max_price_label')
-        self.max_price.setRange(0,1000000)
-        self.max_price.valueChanged.connect(self.updateMaxLabel)
+		#max price
+		self.max_price = self.findChild(QtWidgets.QSlider, 'max_price')
+		self.max_price_label = self.findChild(QtWidgets.QLabel, 'max_price_label')
+		self.max_price.setRange(0,1000000)
+		self.max_price.valueChanged.connect(self.updateMaxLabel)
 
-        #action buttons
-        #search facts button
-        self.search_facts =  self.findChild(QtWidgets.QPushButton, 'search_facts')
-        self.search_facts.clicked.connect(self.searchFactsClickListener)
+		#action buttons
+		#search facts button
+		self.search_facts =  self.findChild(QtWidgets.QPushButton, 'search_facts')
+		self.search_facts.clicked.connect(self.searchFactsClickListener)
 
-        #remove from fact list button 
-        self.remove_fact =  self.findChild(QtWidgets.QPushButton, 'remove_fact')
-        self.remove_fact.clicked.connect(self.removeFactClickListener)
+		#remove from fact list button 
+		self.remove_fact =  self.findChild(QtWidgets.QPushButton, 'remove_fact')
+		self.remove_fact.clicked.connect(self.removeFactClickListener)
 
-        #reset fact list button 
-        self.reset_facts =  self.findChild(QtWidgets.QPushButton, 'reset_facts')
-        self.reset_facts.clicked.connect(self.resetFactsClickListener)
+		#reset fact list button 
+		self.reset_facts =  self.findChild(QtWidgets.QPushButton, 'reset_facts')
+		self.reset_facts.clicked.connect(self.resetFactsClickListener)
 
-        #add to cart button
-        self.add_to_cart =  self.findChild(QtWidgets.QPushButton, 'add_to_cart')
-        self.add_to_cart.clicked.connect(self.addToCartClickListener)
+		#add to cart button
+		self.add_to_cart =  self.findChild(QtWidgets.QPushButton, 'add_to_cart')
+		self.add_to_cart.clicked.connect(self.addToCartClickListener)
 
-        #remove from cart button
-        self.remove_from_cart =  self.findChild(QtWidgets.QPushButton, 'remove_from_cart')
-        self.remove_from_cart.clicked.connect(self.removeFromCartClickListener)
+		#remove from cart button
+		self.remove_from_cart =  self.findChild(QtWidgets.QPushButton, 'remove_from_cart')
+		self.remove_from_cart.clicked.connect(self.removeFromCartClickListener)
 
-        #purchase from cart button
-        self.purchase =  self.findChild(QtWidgets.QPushButton, 'purchase')
-        self.purchase.clicked.connect(self.purchaseClickListener)
+		#purchase from cart button
+		self.purchase =  self.findChild(QtWidgets.QPushButton, 'purchase')
+		self.purchase.clicked.connect(self.purchaseClickListener)
 
-        #reset cart button
-        self.reset_cart =  self.findChild(QtWidgets.QPushButton, 'reset_cart')
-        self.reset_cart.clicked.connect(self.resetCartClickListener)
+		#reset cart button
+		self.reset_cart =  self.findChild(QtWidgets.QPushButton, 'reset_cart')
+		self.reset_cart.clicked.connect(self.resetCartClickListener)
 
-        self.show()
-    
-    #button click listeners
-    ################################################## vehicles ###################################################
-    def updateMinLabel(self, value):
-        self.min_price_label.setText(str(value))
-    def updateMaxLabel(self, value):
-        self.max_price_label.setText(str(value))
-    def addSizeClickListener(self):
-        # item = QtGui.QStandardItem(f'wheels = {self.wheels.value()}')
-        # self.listModel.appendRow(item)
-        pass
-    def addDoorsClickListener(self):
-        # item = QtGui.QStandardItem(f'doors = {self.doors.value()}')
-        # self.listModel.appendRow(item)
-        pass
-    def addMaxTempClickListener(self):
-        # item = QtGui.QStandardItem(f'motor = {self.motor.currentText()}')
-        # self.listModel.appendRow(item)
-        pass
-    def addMinTempClickListener(self):
-        # item = QtGui.QStandardItem(f'size = {self.size.currentText()}')
-        # self.listModel.appendRow(item)
-        pass
-    def addMaxPriceClickListener(item):
-        pass  
-    def addFeatureClickListener(item):
-        pass 
-    def addAccessoryClickListener(item):
-        pass   
-    def addMinPriceClickListener(self):
-        # if len(self.FactListVehicles.selectedIndexes()) >= 1:
-        #     for items in reversed(sorted(self.FactListVehicles.selectedIndexes())):
-        #         self.listModel.takeRow(items.row()) 
-        pass
-    def resetFactsClickListener(self):
-        # self.listModel.removeRows( 0, self.listModel.rowCount() )
-        pass
-    def searchFactsClickListener(self):
-        # for index in range(self.listModel.rowCount()):
-        #     fect = f'engine.declare(Facts({self.listModel.item(index).text()}))'
-        #     exec(fect)
-        # engineV.run()
-        # print(engineV.facts)
-        pass
-    def resetCartClickListener(self):
-        # self.listModel.removeRows( 0, self.listModel.rowCount() )
-        pass
-    def removeFactClickListener(self):
-        # if len(self.FactListVehicles.selectedIndexes()) >= 1:
-        #     for items in reversed(sorted(self.FactListVehicles.selectedIndexes())):
-        #         self.listModel.takeRow(items.row()) 
-        pass
-    def purchaseClickListener(self):
-        pass
-    def removeFromCartClickListener(self):
-        # if len(self.FactListVehicles.selectedIndexes()) >= 1:
-        #     for items in reversed(sorted(self.FactListVehicles.selectedIndexes())):
-        #         self.listModel.takeRow(items.row()) 
-        pass
-    def addToCartClickListener(self):
-        pass
-        
-        
+		self.show()
+	
+	#button click listeners
+	################################################## vehicles ###################################################
+	def updateMinLabel(self, value):
+		self.min_price_label.setText(str(value))
+	def updateMaxLabel(self, value):
+		self.max_price_label.setText(str(value))
+	def addSizeClickListener(self):
+		item = QtGui.QStandardItem(f'size={self.combo_size.currentText()}')
+		self.facts_list_model.appendRow(item)
+		
+	def addDoorsClickListener(self):
+		item = QtGui.QStandardItem(f'doors = {self.spinner_door.value()}')
+		self.facts_list_model.appendRow(item)
+		
+	def addMaxTempClickListener(self):
+		item = QtGui.QStandardItem(f'hi_temp = {self.max_temp_spinner.value()}')
+		self.facts_list_model.appendRow(item)
+	def addMinTempClickListener(self):
+		item = QtGui.QStandardItem(f'lo_temp = {self.min_temp_spinner.value()}')
+		self.facts_list_model.appendRow(item)
+	def addMaxPriceClickListener(self):
+		item = QtGui.QStandardItem(f'max_price = {self.max_price.value()}')
+		self.facts_list_model.appendRow(item)
+	def addFeatureClickListener(self):
+		string = str(self.combo_features.currentText())
+		string = string.replace(" ", "")
+		string = string.lower()
+		item = QtGui.QStandardItem(f'features={{"{string}":1}}')
+		self.facts_list_model.appendRow(item)
+	def addAccessoryClickListener(self):
+		string = str(self.combo_accessories.currentText())
+		item = QtGui.QStandardItem(f'accessories={string}')
+		self.facts_list_model.appendRow(item)  
+	def addMinPriceClickListener(self):
+		item = QtGui.QStandardItem(f'min_price = {self.min_price.value()}')
+		self.facts_list_model.appendRow(item)
+		# if len(self.FactListVehicles.selectedIndexes()) >= 1:
+		#     for items in reversed(sorted(self.FactListVehicles.selectedIndexes())):
+		#         self.listModel.takeRow(items.row()) 
+		
+	def resetFactsClickListener(self):
+		self.facts_list_model.removeRows( 0, self.facts_list_model.rowCount() )
+		
+	def searchFactsClickListener(self):
+		# for index in range(self.listModel.rowCount()):
+		#     fect = f'engine.declare(Facts({self.listModel.item(index).text()}))'
+		#     exec(fect)
+		# engineV.run()
+		# print(engineV.facts)
+		pass
+	def resetCartClickListener(self):
+		self.cart.removeRows( 0, self.cart.rowCount() )
+		
+	def removeFactClickListener(self):
+		# if len(self.FactListVehicles.selectedIndexes()) >= 1:
+		#     for items in reversed(sorted(self.FactListVehicles.selectedIndexes())):
+		#         self.listModel.takeRow(items.row()) 
+		pass
+	def purchaseClickListener(self):
+		pass
+	def removeFromCartClickListener(self):
+		# if len(self.FactListVehicles.selectedIndexes()) >= 1:
+		#     for items in reversed(sorted(self.FactListVehicles.selectedIndexes())):
+		#         self.listModel.takeRow(items.row()) 
+		pass
+	def addToCartClickListener(self):
+		pass
+		
+		
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
